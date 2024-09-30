425cd40ddace098d70905ae8d053475a358fb190
sofya <Sofya.k116@mail.ru>
2024-09-30 14:35:34 +0300

    tests

---
+++ hand.py
@@ -0,0 +1,39 @@
+import typing
+from src.card import Card
+class Hand:
+    def __init__(self, cards:  list[Card] = []):
+        if cards is None:
+            # может быть пустая рука
+            cards = []
+        self.cards: list[Card] = cards
+
+    def __repr__(self):
+        return self.save()
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            other = Hand.load(other)
+        return self.cards == other.cards
+
+    def save(self) -> str:
+        scards = [c.save() for c in self.cards]
+        s = ' '.join(scards)
+        return s
+
+    @classmethod
+    def load(cls, text: str) -> typing.Self:
+        cards = [Card.load(s) for s in text.split()]
+        return cls(cards=cards)
+
+    def add_card(self, card: Card):
+        self.cards.append(card)
+
+    def remove_card(self, card: Card):
+        self.cards.remove(card)
+
+    def score(self):
+        """Штрафные очки"""
+        res = 0
+        for c in self.cards:
+            res += c.score()
+        return res
