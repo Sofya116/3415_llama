f269347573b606792849f109cba0419f9cd10b0e
sofya <Sofya.k116@mail.ru>
2024-10-29 14:34:25 +0300

    test deck

---
+++ tests/test_deck.py
@@ -0,0 +1,68 @@
+import random
+
+from card import lamaCard
+from deck1 import Deck
+
+cards = [lamaCard(3), lamaCard(5), lamaCard(6)]
+
+
+def test_init():
+    d = Deck(cards=cards)
+    assert d.cards == cards
+
+
+def test_init_shuffle():
+    """Проверяем, что карт столько же, но они в другом порядке."""
+    full_deck1 = Deck(None)
+    full_deck2 = Deck(None)
+    assert full_deck1.cards != full_deck2.cards
+
+
+def test_save():
+    d = Deck(cards=cards)
+    assert d.save() == "3 5 6"
+
+    d = Deck(cards=[])
+    assert d.save() == ""
+
+
+def test_load():
+    d = Deck.load("3 5 6")
+    expected_deck = Deck(cards)
+
+    assert d == expected_deck
+
+
+def test_draw_card():
+    d1 = Deck.load("3 5 6")
+    d2 = Deck.load("3 5")
+    c = d1.draw_card()
+    assert c == lamaCard.load("6")
+    assert d1 == d2
+
+
+def test_shuffle_1():
+    test_cards = lamaCard.all_cards(numbers=[2, 4, 6])
+    deck = Deck(cards=test_cards)
+    deck_list = [deck.save()]
+    for i in range(5):
+        deck.shuffle()
+        s = deck.save()
+        assert s not in deck_list
+        deck_list.append(s)
+
+
+def test_shuffle_2():
+    random.seed(3)
+
+    test_cards = lamaCard.all_cards(numbers=[4, 2, 6])
+    deck = Deck(cards=test_cards)
+
+    deck.shuffle()
+    assert deck.save() == "2 6 4"
+
+    deck.shuffle()
+    assert deck.save() == "2 4 6"
+
+    deck.shuffle()
+    assert deck.save() == "4 2 6"
