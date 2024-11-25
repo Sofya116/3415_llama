75df14acc414604d41da58f081b1b44034bd1975
sofya <Sofya.k116@mail.ru>
2024-11-25 13:46:29 +0300

    test deck

---
+++ main/player.py
@@ -0,0 +1,50 @@
+import hashlib
+import json
+import typing
+from hand import Hand
+
+
+class Player:
+    #Инициализация игрока
+    def __init__(self, name: str, hand: Hand, score: int = 0):
+        self.name = name
+        self.hand = hand
+        self.score = score
+
+    def __str__(self):
+        return f"{self.name}({self.score}): {self.hand}"
+
+    def __eq__(self, other: typing.Union['Player', str, dict]):
+        if isinstance(other, str):
+            other = self.load(json.loads(other))
+        if isinstance(other, dict):
+            other = self.load(other)
+        return (
+                self.name == other.name
+                and self.score == other.score
+                and self.hand == other.hand
+        )
+
+    def __hash__(self) -> int:
+        return int(hashlib.sha1(self.name.encode("utf-8")).hexdigest(), 16) % (10 ** 8)
+
+    #Сохраняет состояние игрока в словарь.
+    def save(self) -> dict:
+        return {
+            "name": self.name,
+            "hand": self.hand.save(),
+            "score": self.score
+        }
+
+    @classmethod
+    #Загружает игрока из словаря
+    def load(cls, data: dict):
+        if not all(key in data for key in ["name", "hand", "score"]):
+            raise ValueError("Missing keys in data for Player loading.")
+
+        return cls(
+            name=data["name"],
+            hand=Hand.load(data["hand"]),
+            score=int(data["score"])
+        )
+
---
+++ tests/test_player.py
@@ -0,0 +1,56 @@
+from hand import Hand
+from player import Player
+from card import lamaCard
+
+def test_init():
+    h = Hand.load("1 3 2")
+    p = Player(name="Jordan", hand=h, score=10)
+    assert p.name == "Jordan"
+    assert p.hand == h
+    assert p.score == 10
+
+def test_str():
+    h = Hand.load("1 3 2")
+    p = Player(name="Jordan", hand=h, score=15)
+    assert str(p) == "Jordan(15): 1 3 2"
+
+def test_save():
+    h = Hand.load("1 3 2")
+    p = Player(name="Jordan", hand=h, score=15)
+    assert p.save() == {"name": "Jordan", "score": 15, "hand": "1 3 2"}
+
+def test_eq():
+    h1 = Hand.load("1 3 2")
+    h2 = Hand.load("1 3 2")
+    p1 = Player(name="Jordan", hand=h1, score=10)
+    p2 = Player(name="Jordan", hand=h2, score=10)
+    assert p1 == p2
+
+def test_load():
+    data = {"name": "Jordan", "score": 10, "hand": "1 3 2"}
+    h = Hand.load("1 3 2")
+    p_expected = Player(name="Jordan", hand=h, score=10)
+    p = Player.load(data)
+    assert p == p_expected
+
+def test_hash():
+    h = Hand.load("1 3 2")
+    p = Player(name="Jordan", hand=h, score=10)
+    assert isinstance(hash(p), int)
+
+def test_hand_modification():
+    h = Hand.load("1 3 2")
+    p = Player(name="Jordan", hand=h, score=10)
+    p.hand.add_card(lamaCard.load("4"))
+    assert len(p.hand.cards) == 4
+    assert str(p) == "Jordan(10): 1 3 2 4"
+
+def test_hand_score():
+    h = Hand.load("1 3 2")
+    p = Player(name="Jordan", hand=h, score=10)
+    p.hand.add_card(lamaCard.load("Lama"))  # Добавляем карту Лама
+    assert p.hand.score() == 16  # Проверяем, что сумма очков в руке равна 16 (3*1 + 10)
+
+def test_llama_card_score():
+    lama_card = lamaCard.load("Lama")
+    assert lama_card.score() == 10  # Проверяем, что карта Лама дает 10 очков
