f34ad4212ec4c8f6481a536073e3e99de857cd7f
sofya <Sofya.k116@mail.ru>
2024-11-26 10:59:47 +0300

    Доработанный player

--- main/player.py
+++ main/player.py
@@ -3,9 +3,8 @@ import json
 import typing
 from hand import Hand
 
-
 class Player:
-    #Инициализация игрока
+    # Инициализация игрока
     def __init__(self, name: str, hand: Hand, score: int = 0):
         self.name = name
         self.hand = hand
@@ -15,30 +14,33 @@ class Player:
         return f"{self.name}({self.score}): {self.hand}"
 
     def __eq__(self, other: typing.Union['Player', str, dict]):
+        # Сравнение игрока с другим объектом
         if isinstance(other, str):
             other = self.load(json.loads(other))
         if isinstance(other, dict):
             other = self.load(other)
         return (
-                self.name == other.name
-                and self.score == other.score
-                and self.hand == other.hand
+            self.name == other.name
+            and self.score == other.score
+            and self.hand == other.hand
         )
 
     def __hash__(self) -> int:
+        # Возвращает хэш-код игрока
         return int(hashlib.sha1(self.name.encode("utf-8")).hexdigest(), 16) % (10 ** 8)
 
-    #Сохраняет состояние игрока в словарь.
+    # Сохраняет состояние игрока в словарь
     def save(self) -> dict:
         return {
             "name": self.name,
-            "hand": self.hand.save(),
+            "hand": self.hand.save(),  # Предполагается, что метод save() возвращает сериализованное представление руки
             "score": self.score
         }
 
     @classmethod
-    #Загружает игрока из словаря
+    # Загружает игрока из словаря
     def load(cls, data: dict):
+        # Проверка наличия необходимых ключей в словаре
         if not all(key in data for key in ["name", "hand", "score"]):
             raise ValueError("Missing keys in data for Player loading.")
 
